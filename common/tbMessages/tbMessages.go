c69054703ddf5704a33c766e34e4bd35cc042854
igismo <scuric@isi.edu>
2019-12-13 16:57:22 -0800

    dec 10 changes

--- common/tbMessages/tbMessages.go
+++ common/tbMessages/tbMessages.go
@@ -1,4 +1,3 @@
-
 //=============================================================================
 // FILE NAME: tbMessages.go
 // DESCRIPTION:
@@ -9,170 +8,189 @@
 //================================================================================
 
 package tbMessages
-import (
-"net"
 
+import (
+	"net"
 )
+
 //      "os/user"
 type TBmessage struct {
-	MsgReceiver  NameId
-	MsgSender    NameId
+	MsgReceiver NameId
+	MsgSender   NameId
 	// MsgClass  string
-	MsgType      string
-	TimeSent     string      // msg send time
-	MsgBody      [] byte // Message type specific body
+	MsgType  string
+	TimeSent string // msg send time
+	MsgBody  []byte // Message type specific body
 }
 
 type TBmessageExpCreate struct {
-	MsgReceiver  NameId
-	MsgSender    NameId
+	MsgReceiver NameId
+	MsgSender   NameId
 	// MsgClass  string
-	MsgType      string
-	TimeSent     string      // msg send time
-	MsgBody      ExpCreate // Message type specific body
+	MsgType  string
+	TimeSent string    // msg send time
+	MsgBody  ExpCreate // Message type specific body
 }
 
 type MsgData interface {
-
 	area() float64
 	perim() float64
 }
 
 type TBmgr struct {
-	Name                    NameId
-	Up                      bool
-	LastChangeTime          string
-	MsgsSent                int64
-	LastSentAt              string
-	MsgsRcvd                int64
-	LastRcvdAt              string
+	Name           NameId
+	Up             bool
+	LastChangeTime string
+	MsgsSent       int64
+	LastSentAt     string
+	MsgsRcvd       int64
+	LastRcvdAt     string
 }
 
 type NameId struct {
-	Name         string  // Name String
-	OsId         int     // Task id, if known
-	TimeCreated  string      // my incarnation time
-	Address      net.UDPAddr
+	Name        string // Name String
+	OsId        int    // Task id, if known
+	TimeCreated string // my incarnation time
+	Address     net.UDPAddr
 }
 
+type LinuxCommand struct {
+	Cmd  string
+	Par1 string
+	Par2 string
+	Par3 string
+	Par4 string
+	Par5 string
+	Par6 string
+}
+type CommandList []LinuxCommand
+type SatRouteTableChange []CommandList   // each set is per sat, list of commands
+type ConstPosition []SatRouteTableChange // one set per sattelite
 
 // Message source or destination
 //----------------------------------------------------------------------------
-const EXP_MGR     = "ExpMgr"
-const RSRC_MGR    = "RsrcMgr"
-const OFFICE_MGR  = "OfficeMgr"
+const EXP_MGR = "ExpMgr"
+const RSRC_MGR = "RsrcMgr"
+const OFFICE_MGR = "OfficeMgr"
 
 // Message Classes and types within classes:
-const  MSG_CLASS_CONTROL       = "MSG_CLASS_CONTROL"
+const MSG_CLASS_CONTROL = "MSG_CLASS_CONTROL"
 
 // Format of message body part, if any specific data ....
 //----------------------------------------------------------------------------
-const  MSG_TYPE_INIT           = "MSG_INIT"
-type  MsgInit struct {
+const MSG_TYPE_INIT = "MSG_INIT"
+
+type MsgInit struct {
 	//
 }
-const  MSG_TYPE_REGISTER     = "REGISTER"
-type  MsgRegister struct {
+
+const MSG_TYPE_REGISTER = "REGISTER"
+
+type MsgRegister struct {
 	Mgr TBmgr
 }
+
 const MSG_TYPE_SAT_STATUS = "SAT STATUS"
+
 type MsgSatStatus struct {
-	Mgr TBmgr
+	Mgr       TBmgr
 	SatStatus string
 }
-const MSG_TYPE_CMD = "COMMAND"
+
+const MSG_TYPE_CMD = "COMMANDS"
+
 type MsgCmd struct {
-	Mgr TBmgr
-	Cmd string
+	Mgr  TBmgr
+	cmds []LinuxCommand
 }
+
 const MSG_TYPE_CMD_REPLY = "CMD_REPLY"
+
 type MsgCmdReply struct {
-	Mgr TBmgr
+	Mgr      TBmgr
 	CmdReply string
 }
-const  MSG_TYPE_KEEPALIVE     = "KEEPALIVE"
-type  MsgKeepAlkive struct {
-	tableOfMgrs [] TBmgr
-}
-const  MSG_TYPE_HELLO     = "HELLO"
-type  MsgHello struct {
-	tableOfMgrs [] TBmgr
-}
-const  MSG_TYPE_HELLO_REPPLY     = "HELLO_REPLY"
-type  MsgHelloReply struct {
 
-}
-const  MSG_TYPE_CONNECT              = "CONNECT"
-type  MsgConnect struct {
+const MSG_TYPE_KEEPALIVE = "KEEPALIVE"
 
-}
-const  MSG_TYPE_CONNECTING     = "CONNECTING"
-type  MsgConnecting struct {
-
-}
-const  MSG_TYPE_CONNECTED      = "CONNECTED"
-type  MsgConnected struct {
-
-}
-const  MSG_TYPE_DISCONNECT         = "DISCONNECT"
-type  MsgDisconnect struct {
-
-}
-const  MSG_TYPE_DISCONNECTING   = "DISCONNECTING"
-type  MsgDisconnecting struct {
-
-}
-const  MSG_TYPE_DISCONNECTED   = "MSG_DISCONNECTED"
-type  MsgDisconnected struct {
-
-}
-const  MSG_TYPE_TERMINATE      = "MSG_TERMINATE"
-type  MsgTerminate struct {
-
-}
-const  MSG_TYPE_TERMINATING    = "MSG_TERMINATING"
-type  MsgTerminating struct {
-
-}
-const  MSG_TYPE_TERMINATED     = "MSG_TERMINATED"
-type  MsgTerminated struct {
-
+type MsgKeepAlive struct {
+	tableOfMgrs []TBmgr
 }
 
-const  MSG_CLASS_RSRCMGR       = "MSG_CLASS_RSRCMGR"
+const MSG_TYPE_HELLO = "HELLO"
+
+type MsgHello struct {
+	tableOfMgrs []TBmgr
+}
+
+const MSG_TYPE_HELLO_REPPLY = "HELLO_REPLY"
+
+type MsgHelloReply struct {
+}
+
+const MSG_TYPE_CONNECT = "CONNECT"
+
+type MsgConnect struct {
+}
+
+const MSG_TYPE_CONNECTING = "CONNECTING"
+
+type MsgConnecting struct {
+}
+
+const MSG_TYPE_CONNECTED = "CONNECTED"
+
+type MsgConnected struct {
+}
+
+const MSG_TYPE_DISCONNECT = "DISCONNECT"
+
+type MsgDisconnect struct {
+}
+
+const MSG_TYPE_DISCONNECTING = "DISCONNECTING"
+
+type MsgDisconnecting struct {
+}
+
+const MSG_TYPE_DISCONNECTED = "MSG_DISCONNECTED"
+
+type MsgDisconnected struct {
+}
+
+const MSG_TYPE_TERMINATE = "MSG_TERMINATE"
+
+type MsgTerminate struct {
+}
+
+const MSG_TYPE_TERMINATING = "MSG_TERMINATING"
+
+type MsgTerminating struct {
+}
+
+const MSG_TYPE_TERMINATED = "MSG_TERMINATED"
+
+type MsgTerminated struct {
+}
+
+const MSG_CLASS_RSRCMGR = "MSG_CLASS_RSRCMGR"
+
 //----------------------------------------------------------------------------
 
+const MSG_CLASS_OFFICEMGR = "MSG_CLASS_OFFICEMGR"
 
-const  MSG_CLASS_OFFICEMGR     = "MSG_CLASS_OFFICEMGR"
 //----------------------------------------------------------------------------
-const  MSG_TYPE_EXPCREATE           = "EXPCREATE"
-type  ExpCreate struct {
-	Project         string // $exp_pid
-	Experiment      string // $exp_id
-	GroupId         string // $exp_gid
-	LinktestArgs    string // $linktestarg
-	ExtraGroups     string // $extragroups
-	ExpSwappable    string // $exp_swappable
-	ExpDesc         string // $exp_desc
-	BatchArgs       string // $batcharg
-	UserName        string // $uid
-	FileName        string // $thensfile
+const MSG_TYPE_EXPCREATE = "EXPCREATE"
+
+type ExpCreate struct {
+	Project      string // $exp_pid
+	Experiment   string // $exp_id
+	GroupId      string // $exp_gid
+	LinktestArgs string // $linktestarg
+	ExtraGroups  string // $extragroups
+	ExpSwappable string // $exp_swappable
+	ExpDesc      string // $exp_desc
+	BatchArgs    string // $batcharg
+	UserName     string // $uid
+	FileName     string // $thensfile
 }
-
-
-
-const  MSG_TYPE_SWAPIN           = "SWAPIN"
-type  SwapIn struct {
-	Project         string
-	Experiment      string
-	UserName        string
-	FileName        string
-}
-const  MSG_TYPE_SWAPOUT           = "SWAPOUT"
-type  SwapOut struct {
-	Project         string
-	Experiment      string
-	UserName        string
-	FileName        string
-}
-
